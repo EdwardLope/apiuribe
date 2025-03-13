7ae370ec89e637c2d22d7684d924fc7617079b09
carlosurango03 <carlosaoviedo03@gmail.com>
2025-03-12 19:21:09 -0500

    se realizan cambios

--- app/api/endpoins/endpoins.py
+++ app/api/endpoins/endpoins.py
@@ -6,14 +6,31 @@ from fastapi.params import Depends
 from app.api.DTO.dtos import PoveedorDTO,PoveedorDTOEnvio,LogisticaDTO,LogisticaDTOEnvio
 from app.api.models.tablas import Poveedor,Logistica
 
+from app.database.connection import SessionLocal, engine
+
 rutas=APIRouter()
+
 #Rutinas para conectarse a la bese de datos 
 
-#Rutina para construir servicion wed
-
-def guardarPoveedor(datosPoveedor):
+def conectarConBD ():
     try:
-        Poveedor(
+
+        baseDatos=SessionLocal()
+        yield baseDatos
+
+    except Exception as error:
+        baseDatos.roback()
+        raise error
+
+    finally:
+        baseDatos.close()
+
+#Rutina para construir servicion wed
+@rutas.post("/proveedor",response_model=PoveedorDTOEnvio,summary="servicio para guardar un proveedor en la base de datos ")
+
+def guardarPoveedor(datosPoveedor:PoveedorDTO,database:Session=Depends(conectarConBD)):
+    try:
+        proveedorAGuardar=Poveedor(
             nombres=datosPoveedor.nombres,
             documento=datosPoveedor.documento,
             Direccion=datosPoveedor.Direccion,
@@ -25,14 +42,18 @@ def guardarPoveedor(datosPoveedor):
             costoDeEnvio=datosPoveedor.costoDeEnvio,
             Descripcion=datosPoveedor.Descripcion
         )
-        
+        database.add(proveedorAGuardar)
+        database.commit()
+        database.refresh(proveedorAGuardar)
+        return proveedorAGuardar
     
 
     except Exception as error:
-        pass 
+        database.rollback()
+        raise HTTPException(status_code=400,detail=("tenemos un error {error}"))
 
 
-def guardarLogistica(datosLogistica):
+def guardarLogistica(datosLogistica:LogisticaDTO):
     try:
         Logistica(
             nombreEncargado=datosLogistica.nombreEncargado,
